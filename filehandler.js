79c342e2bfb1f88d118e8d5d6d3fc8acc087df07
masp1234 <mart2c04@stud.kea.dk>
2022-11-17 12:51:43 +0100

    can read files and display pictures

--- filehandler.js
+++ filehandler.js
@@ -1,19 +1,31 @@
-const fileUploadForm = document.getElementById("file-upload-form");
 
 // skal selvfølgelig laves anderledes
 const userId = 1;
 
+let imageURLString = "";
 
-fileUploadForm.addEventListener('submit', async e => {
-    // vigtig, men kan ikke huske hvorfor
-    e.preventDefault();
 
-    const files = document.getElementById('file').files;
 
-    for (const file of files) {
-        postFile(file);
+const inputFiles = document.querySelector('input[type="file"]');
+inputFiles.addEventListener('change', (e) => {
+    console.log(inputFiles.files);
+    const reader = new FileReader();
+    reader.onload = () => {
+        console.log(reader.result);
+        imageURLString = reader.result.replace("data:", "")
+                                        .replace(/^.+,/, "");
+            const image = new Image();
+            image.setAttribute('src', "data:image/jpg;base64," + imageURLString);
+            document.body.appendChild(image);
+            
     }
-})
+    reader.readAsDataURL(inputFiles.files[0]);
+    
+    
+
+}, false)
+
+   
 
 const postFile = async file => {
     const formData = new FormData();
