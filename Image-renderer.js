be5060f85073b596e06a8cc8976db34c2225bb3f
masp1234 <mart2c04@stud.kea.dk>
2022-11-18 11:55:53 +0100

    add some css to image rendering

--- Image-renderer.js
+++ Image-renderer.js
@@ -10,11 +10,15 @@ const renderImages = async () => {
         data.forEach(image => {
             imageContainer.innerHTML += `  
                 <div class="imageElement">
-                    <img src=${image.image} alt="">
-                    <p>${image.filename}</p>
-                    <p>${image.timeStamp}</p>
-                    <p>${image.originalByteSize}</p>
-                    <p>${image.description}</p>
+                    <div class="image-inner-container">
+                        <img src=${image.image} alt="image not found" width=200>
+                    </div>
+                    <div class="image-text-container">
+                        <p>${image.filename}</p>
+                        <p>${image.timeStamp}</p>
+                        <p>${image.originalByteSize}</p>
+                        <p>${image.description}</p>
+                    </div>
                 </div>
             ` 
         });
